c8f13194922d906e3d1a97290651667b97506570
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 19:42:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,10 +47,6 @@ attr_accessor :name, :grade, :id
   end
   
   def self.new_from_db(row)
-   sql = <<-SQL
-      SELECT * FROM students
-      SQL
-    DB[:conn].execute(sql).map do |row|
       new_student = self.new
       new_student.id = row[0]
       new_student.name = row[1]
