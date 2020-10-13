f6579e98057dd6f0c199f2f552e07a568409ae56
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 19:43:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ attr_accessor :name, :grade, :id
   end
   
   def self.new_from_db(row)
-      new_student = self.new
+      new_student = self.new(row)
       new_student.id = row[0]
       new_student.name = row[1]
       new_student.grade = row[2]
