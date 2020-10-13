4f8362404fa2deb1c4a79afccf49a4306ccb64b3
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 19:39:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,8 +55,8 @@ attr_accessor :name, :grade, :id
       new_student.id = row[0]
       new_student.name = row[1]
       new_student.grade = row[2]
+      new_student
     end
-    new_student
   end
   
   
